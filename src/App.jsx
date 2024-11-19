cb0f1fada4bc38d73d2c8bacf09279d068c41ab6
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-11-20 00:49:00 +0530

    [/signup and /login] route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-import {Navbar, Footer} from './components'
+import {Navbar, Footer, Signup} from './components'
 import { Routes, Route } from 'react-router-dom'
 import {Home, Coin} from './pages'
+import Login from './components/signup/Login'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/coin/:coinId' element={<Coin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
       <Footer />
     </div>
