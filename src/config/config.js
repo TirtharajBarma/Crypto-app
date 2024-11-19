a42af7ca7eb566b5c96be781bb65735b97248f2b
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-11-20 00:51:14 +0530

    configuration of .env file

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,12 @@
 const config = {
     currencyUrl: String(import.meta.env.VITE_CURRENCY_API),
+    apiKey: String(import.meta.env.VITE_apiKey),
+    authDomain: String(import.meta.env.VITE_authDomain),
+    projectId: String(import.meta.env.VITE_projectId),
+    storageBucket: String(import.meta.env.VITE_storageBucket),
+    messagingSenderId: String(import.meta.env.VITE_messagingSenderId),
+    appId: String(import.meta.env.VITE_VITE_appId),
+
 }
 
 export default config
