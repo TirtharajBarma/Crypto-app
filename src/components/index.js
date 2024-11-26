dd3b882ed0ad75057d6739987f8506656725aebd
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-11-26 22:20:30 +0530

    add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import Navbar from "./navbar/Navbar"
 import Footer from "./footer/Footer"
 import LineChart from "./lineChart/LineChart"
+import Signup from './signup/Signup'
+import Login from './signup/Login'
 
 export {
     Navbar,
     Footer,
-    LineChart
+    LineChart,
+    Signup,
+    Login
 }
